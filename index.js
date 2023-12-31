33ef57044e295e96b84b5f2f5e63e616fed506ba
Verônica Santana <santanaveronica858@gmail.com>
2023-12-31 15:07:11 -0300

    nova funcionalidade

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-var card = window.document.getElementById('card')
-var icon1 = window.document.getElementById('icon1')
-var icon2 = window.document.getElementById('icon2')
+var card = window.document.getElementById('card');
+var icon1 = window.document.getElementById('icon1');
+var icon2 = window.document.getElementById('icon2');
+var link = window.document.getElementById('link');
 
-function Honda(){
-    card.src = 'imagens/honda.svg'
-    icon1.setAttribute('onclick', 'scootter()')
-    icon2.setAttribute('onclick', 'scootter()')
+function Honda() {
+    card.src = 'imagens/honda.svg';
+    setLinks('https://wa.me/5571987566959?text=quero+saber+mais+sobre+a+Honda+CX500');
 }
 
-function scootter(){
-    card.src = 'imagens/voltz.svg'
-    icon1.setAttribute('onclick', 'Honda()')
-    icon2.setAttribute('onclick', 'Honda()')
+function scootter() {
+    card.src = 'imagens/voltz.svg';
+    setLinks('https://wa.me/5571987566959?text=Quero+saber+sobre+a+volta+ev+1');
 }
 
+function primeirolink() {
+    card.src = 'imagens/honda.svg';
+    setLinks('https://wa.me/5571987566959?text=quero+saber+mais+sobre+a+Honda+CX500');
+}
+
+function segundolink() {
+    card.src = 'imagens/voltz.svg';
+    setLinks('https://wa.me/5571987566959?text=Quero+saber+sobre+a+volta+ev+1');
+}
+
+function setLinks(href) {
+    link.href = href;
+    icon1.setAttribute('onclick', 'scootter()');
+    icon2.setAttribute('onclick', 'scootter()');
+    // Adiciona uma pequena pausa antes de redefinir os links
+    setTimeout(() => {
+        icon1.setAttribute('onclick', 'primeirolink()');
+        icon2.setAttribute('onclick', 'primeirolink()');
+    }, 100);
+}
